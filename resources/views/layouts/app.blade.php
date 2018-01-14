4590e6cfa07f220e1bf37a692261eb0ac4f10e34
WannaProGrammer <1150976163@qq.com>
2018-01-14 20:25:30 +0800

    meta

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -9,7 +9,7 @@
     <meta name="csrf-token" content="{{ csrf_token() }}">
     <meta name="apiToken" content="{{Auth::check()?'Bearer '.Auth::user()->api_token:'Bearer '}}">
     <meta name="description" content="itplatform" />
-
+    {{--<meta name="description" content="itplatform" />--}}
 
     <title>{{ config('app.name') }}</title>
 
